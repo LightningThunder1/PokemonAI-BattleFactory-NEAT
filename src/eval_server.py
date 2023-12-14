37743fe0557d3c822d3e7ffd0c5a609eadb8a130
JavenZ <javen.william@gmail.com>
2023-12-14 15:50:45 -0500

    impl spawn_agent()

--- src/eval_server.py
+++ src/eval_server.py
@@ -1,23 +1,27 @@
 import socket
-import neat
 from neat.nn import FeedForwardNetwork
+import subprocess
 
 
 class EvaluationServer:
 
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+    EMU_PATH = '/home/javen/Desktop/PokeDS/BizHawk-2.9.1-linux-x64/EmuHawkMono.sh'
 
     def __init__(self):
         pass
 
     def eval_genome(self, genome: FeedForwardNetwork) -> float:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Initializing socket server...")
             # bind socket server
             s.bind((self.HOST, self.PORT))
             s.listen()
 
             # spawn agent
+            self.spawn_agent()
+            print("Spawned agent.")
 
             # wait for agent to connect to socket
             conn, addr = s.accept()
@@ -30,3 +34,16 @@ class EvaluationServer:
                         break
                     conn.sendall(data)
         return 0.0
+
+    def spawn_agent(self) -> None:
+        """
+        Spawns the emulator process and starts the eval_client.lua script.
+        :return: None
+        """
+        subprocess.Popen([
+            self.EMU_PATH,
+            f'--socket_port={self.PORT}',
+            f'--socket_ip={self.HOST}',
+            '--lua=./src/eval_client.lua'
+        ])
+
