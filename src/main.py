065196d0bae4fd8890e035457a5247ebac0bbe57
JavenZ <javen.william@gmail.com>
2023-12-14 09:44:52 -0500

    init project

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import os
+import neat
+
+
+class Trainer:
+    def __init__(self):
+        pass
+
+    def eval_genomes(genomes, config):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
