68ac5d12e738059cb0881eec8ee8616c184d1b24
JavenZ <javen.william@gmail.com>
2023-12-14 10:04:55 -0500

    impl template EvaluationServer

---
+++ src/EvaluationServer.py
@@ -0,0 +1,22 @@
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+class EvaluationServer:
+    def __init__(self):
+        pass
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
