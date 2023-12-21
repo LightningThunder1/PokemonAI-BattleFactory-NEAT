0bd4f3ceb8844e11e5716e42603bbc739d8bd9ed
JavenZ <javen.william@gmail.com>
2023-12-21 03:29:24 -0500

    impl read_pokemon()

--- src/eval_client.lua
+++ src/eval_client.lua
@@ -102,96 +102,94 @@ local function numberToBinary(x)
 end
 
 
--- ### POINTERS ###
-gameID = memory.read_s32_le(0x23FFE0C) -- 0x45555043
-gp = memory.read_s32_le(0x02101D2C)  -- game pointer
-sbp = gp + 0xCFF4 -- small block pointer
-pbp = sbp + 0xA0 -- party pokeblock pointer
-
-
 -- ### PARTY POKEMON ###
+local gameID = memory.read_s32_le(0x23FFE0C) -- 0x45555043
+local gp = memory.read_s32_le(0x02101D2C)  -- game pointer
+local sbp = gp + 0xCFF4 -- small block pointer
+local pbp = sbp + 0xA0 -- party pokeblock pointer
 
--- party pokemon #1
-pbp = pbp + (0xEC * 0) -- offset to pokemon-1 block
-local pid = memory.read_u32_le(pbp + 0x00) -- personality value
-local checksum = memory.read_u16_le(pbp + 0x06)-- pokeblock checksum
-local shift = tostring(((pid & 0x3E000) >> 0xD) % 24) -- data block shift value
-print("Shift Value: ", shift)
-print("Shuffle Order:\n", shuffleOrder[shift])
-print("Checksum: ", checksum)
 
--- decrypt pokemon battle bytes
-local B = decrypt(pid, pbp + 0x88, 50)
+local function read_pokemon(pp, party_idx)
+	-- offset pokemon pointer for party index
+	pp = pp + (0xEC * party_idx)
 
--- decrypt pokemon data bytes
-local D = decrypt(checksum, pbp + 0x08, 64)
+	-- pokemon decryption vars
+	local pid = memory.read_u32_le(pp)
+	local checksum = memory.read_u16_le(pp + 0x06)
+	local shift = tostring(((pid & 0x3E000) >> 0xD) % 24)
 
-local function fetch_Dv(block_offset, var_offset)
-	return D[((block_offset + var_offset) / 2) + 1]
+	-- decrypt pokemon bytes
+	local D = decrypt(checksum, pp + 0x08, 64) -- data
+	local B = decrypt(pid, pp + 0x88, 50) -- battle stats
+
+	local function fetch_Dv(block_offset, var_offset)
+		return D[((block_offset + var_offset) / 2) + 1]
+	end
+
+	local function fetch_Bv(var_offset)
+		return B[((var_offset - 0x88) / 2) + 1]
+	end
+
+	-- calculate shuffled block offsets
+	local a_offset = (shuffleOrder[shift]["A"] - 1) * 0x20
+	local b_offset = (shuffleOrder[shift]["B"] - 1) * 0x20
+	local c_offset = (shuffleOrder[shift]["C"] - 1) * 0x20
+	local d_offset = (shuffleOrder[shift]["D"] - 1) * 0x20
+
+	-- instantiate new pokemon obj and populate vars
+	local pokemon = table.shallow_copy(pokemon_struct)
+	pokemon.ID = fetch_Dv(a_offset, 0x08 - 0x08)
+	pokemon.PID = pid
+	pokemon.HeldItem = fetch_Dv(a_offset, 0x0A - 0x08)
+	pokemon.Ability = fetch_Dv(a_offset, 0x15 - 0x08)
+	pokemon.EXP = fetch_Dv(a_offset, 0x10 - 0x08) -- TODO
+	pokemon.Moves = {
+		{
+			ID = fetch_Dv(b_offset, 0x28 - 0x28) & 0xFFFF,
+			PP = fetch_Dv(b_offset, 0x30 - 0x28) & 0x00FF,
+		};
+		{
+			ID = fetch_Dv(b_offset, 0x2A - 0x28) & 0xFFFF,
+			PP = fetch_Dv(b_offset, 0x30 - 0x28) >> 8,
+		};
+		{
+			ID = fetch_Dv(b_offset, 0x2C - 0x28) & 0xFFFF,
+			PP = fetch_Dv(b_offset, 0x32 - 0x28) & 0x00FF,
+		};
+		{
+			ID = fetch_Dv(b_offset, 0x2E - 0x28) & 0xFFFF,
+			PP = fetch_Dv(b_offset, 0x32 - 0x28) >> 8,
+		};
+	}
+	pokemon.IVs = {  -- TODO
+		HP = 0x0,
+		ATK = 0x0,
+		DEF = 0x0,
+		SPEED = 0x0,
+		SPA = 0x0,
+		SPD = 0x0,
+	}
+	pokemon.EVs = {
+		HP = fetch_Dv(a_offset, 0x18 - 0x08) & 0x00FF,
+		ATK = fetch_Dv(a_offset, 0x18 - 0x08) >> 8,
+		DEF = fetch_Dv(a_offset, 0x1A - 0x08) & 0x00FF,
+		SPEED = fetch_Dv(a_offset, 0x1A - 0x08) >> 8,
+		SPA = fetch_Dv(a_offset, 0x1C - 0x08) & 0x00FF,
+		SPD = fetch_Dv(a_offset, 0x1C - 0x08) >> 8,
+	}
+	pokemon.Stats = {
+		Status = fetch_Bv(0x88) & 0x00FF, -- TODO test
+		Level = fetch_Bv(0x8C) & 0x00FF,
+		HP = fetch_Bv(0x8E) & 0xFFFF,
+		MaxHP = fetch_Bv(0x90) & 0xFFFF,
+		ATK = fetch_Bv(0x92) & 0xFFFF,
+		DEF = fetch_Bv(0x94) & 0xFFFF,
+		SPEED = fetch_Bv(0x96) & 0xFFFF,
+		SPA = fetch_Bv(0x98) & 0xFFFF,
+		SPD = fetch_Bv(0x9A) & 0xFFFF,
+	}
+	return pokemon
 end
-
-local function fetch_Bv(var_offset)
-	return B[((var_offset - 0x88) / 2) + 1]
-end
-
--- calculate shuffled block offsets
-local a_offset = (shuffleOrder[shift]["A"] - 1) * 0x20
-local b_offset = (shuffleOrder[shift]["B"] - 1) * 0x20
-local c_offset = (shuffleOrder[shift]["C"] - 1) * 0x20
-local d_offset = (shuffleOrder[shift]["D"] - 1) * 0x20
-
--- instantiate new pokemon obj and populate vars
-local pokemon = table.shallow_copy(pokemon_struct)
-pokemon.ID = fetch_Dv(a_offset, 0x08 - 0x08)
-pokemon.PID = pid
-pokemon.HeldItem = fetch_Dv(a_offset, 0x0A - 0x08)
-pokemon.Ability = fetch_Dv(a_offset, 0x15 - 0x08)
-pokemon.EXP = fetch_Dv(a_offset, 0x10 - 0x08) -- TODO
-pokemon.Moves = {
-	{
-		ID = fetch_Dv(b_offset, 0x28 - 0x28) & 0xFFFF,
-		PP = fetch_Dv(b_offset, 0x30 - 0x28) & 0x00FF,
-	};
-	{
-		ID = fetch_Dv(b_offset, 0x2A - 0x28) & 0xFFFF,
-		PP = fetch_Dv(b_offset, 0x30 - 0x28) >> 8,
-	};
-	{
-		ID = fetch_Dv(b_offset, 0x2C - 0x28) & 0xFFFF,
-		PP = fetch_Dv(b_offset, 0x32 - 0x28) & 0x00FF,
-	};
-	{
-		ID = fetch_Dv(b_offset, 0x2E - 0x28) & 0xFFFF,
-		PP = fetch_Dv(b_offset, 0x32 - 0x28) >> 8,
-	};
-}
-pokemon.IVs = {  -- TODO
-	HP = 0x0,
-	ATK = 0x0,
-	DEF = 0x0,
-	SPEED = 0x0,
-	SPA = 0x0,
-	SPD = 0x0,
-}
-pokemon.EVs = {
-	HP = fetch_Dv(a_offset, 0x18 - 0x08) & 0x00FF,
-	ATK = fetch_Dv(a_offset, 0x18 - 0x08) >> 8,
-	DEF = fetch_Dv(a_offset, 0x1A - 0x08) & 0x00FF,
-	SPEED = fetch_Dv(a_offset, 0x1A - 0x08) >> 8,
-	SPA = fetch_Dv(a_offset, 0x1C - 0x08) & 0x00FF,
-	SPD = fetch_Dv(a_offset, 0x1C - 0x08) >> 8,
-}
-pokemon.Stats = {
-	Status = fetch_Bv(0x88) & 0x00FF, -- TODO test
-	Level = fetch_Bv(0x8C) & 0x00FF,
-	HP = fetch_Bv(0x8E) & 0xFFFF,
-	MaxHP = fetch_Bv(0x90) & 0xFFFF,
-	ATK = fetch_Bv(0x92) & 0xFFFF,
-	DEF = fetch_Bv(0x94) & 0xFFFF,
-	SPEED = fetch_Bv(0x96) & 0xFFFF,
-	SPA = fetch_Bv(0x98) & 0xFFFF,
-	SPD = fetch_Bv(0x9A) & 0xFFFF,
-}
 
 -- print(pokemon)
 
